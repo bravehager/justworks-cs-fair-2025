24e12f330aaa1b27e37af07354b68c850b835bfc
Nico Coloma-Cook <ncoloma-cook@justworks.com>
2025-03-26 21:59:51 -0400

    wip on badges by borough filter

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,24 @@
 <template>
-  <NuxtLink class="page-link" :to="{ name: 'badges-new' }">
-    Create a new badge
-  </NuxtLink>
+  <div class="row">
+    <NuxtLink class="page-link" :to="{ name: 'badges-new' }">
+      Create a new badge
+    </NuxtLink>
+    <div class="page-link">
+      <label for="borough">Check out Badges by borough</label>
+      <select id="borough" v-model="borough" required>
+        <option value="manhattan">Manhattan</option>
+        <option value="brooklyn">Brooklyn</option>
+        <option value="queens">Queens</option>
+        <option value="the_bronx">The Bronx</option>
+        <option value="staten_island">Staten Island</option>
+      </select>
+    </div>
+  </div>
+
   <BadgeGallery :badges="badges" />
 </template>
 
 <script setup lang="ts">
+const borough = ref("Manhattan");
 const badges = await $fetch("/api/badges");
 </script>
--- server/api/badges.ts
+++ server/api/badges.ts
@@ -1,7 +1,18 @@
+import { eq } from "drizzle-orm";
 import { db } from "~/db";
 import { badgesTable } from "~/db/schema";
 
 export default defineEventHandler(async (event) => {
-  const badges = await db.select().from(badgesTable);
-  return badges;
+  const params = getQuery(event);
+
+  if (params.borough) {
+    const badges = await db
+      .select()
+      .from(badgesTable)
+      .where(eq(badgesTable.borough, String(params.borough)));
+    return badges;
+  } else {
+    const badges = await db.select().from(badgesTable);
+    return badges;
+  }
 });
