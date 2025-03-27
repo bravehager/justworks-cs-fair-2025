c9f2a34a70ecec347a21307747ec237bda2b6295
Brave Hager <bhager@justworks.com>
2025-03-27 19:07:35 -0400

    Use Justworks colors for avatars

--- components/BadgeAvatar.vue
+++ components/BadgeAvatar.vue
@@ -7,10 +7,48 @@ const props = defineProps<{
   name: string;
 }>();
 
+const colors = [
+  "e5f3ff",
+  "b6e5ff",
+  "6dc8ff",
+  "52b0ff",
+  "0092f6",
+  "0070d4",
+  "0e57b5",
+  "1e3d92",
+  "15246d",
+  "f3b715",
+  "ad5e0d",
+  "753204",
+  "643f00",
+  "ddf3ea",
+  "98E2C5",
+  "65c9a3",
+  "00a46b",
+  "008354",
+  "005133",
+  "154d38",
+  "fff0eb",
+  "ff9f82",
+  "fb7f59",
+  "df5a31",
+  "cb431d",
+  "af2c10",
+  "832512",
+  "762615",
+  "f4ebfc",
+  "baaaf9",
+  "8a73e5",
+  "5f43ca",
+  "4529b4",
+  "423674",
+];
+
 const avatarUrl = computed(() => {
   const url = new URL("https://api.dicebear.com/9.x/bottts/svg");
   url.searchParams.set("seed", props.name);
   url.searchParams.set("backgroundColor", "transparent");
+  url.searchParams.set("baseColor", colors.join(","));
   return url.toString();
 });
 </script>
