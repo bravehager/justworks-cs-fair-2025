836c8cb35f066a306597070b600c80e65dea04bb
Nico Coloma-Cook <ncoloma-cook@justworks.com>
2025-03-28 10:17:43 -0400

    fe filtering

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,49 @@
 <template>
-  <NuxtLink class="new-badge button btn-outline" to="/badges/new">
-    <PhosphorIconPlus size="1rem" />
-    New Badge
-  </NuxtLink>
+  <div class="header">
+    <div>
+      <label for="borough">Borough</label>
+      <select id="borough" v-model="borough" required>
+        <option value=""></option>
+        <option value="Manhattan">Manhattan</option>
+        <option value="Brooklyn">Brooklyn</option>
+        <option value="Queens">Queens</option>
+        <option value="The Bronx">The Bronx</option>
+        <option value="Staten Island">Staten Island</option>
+      </select>
+    </div>
+
+    <NuxtLink class="button btn-outline" to="/badges/new">
+      <PhosphorIconPlus size="1rem" />
+      New Badge
+    </NuxtLink>
+  </div>
+
   <div class="badge-gallery">
-    <BadgeGallery :badges="badges" />
+    <BadgeGallery :badges="currentBadges" />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Badge } from "~/db/schema";
-
+const borough = ref("");
 const badges = await $fetch<Badge[]>("/api/badges");
+
+const currentBadges = computed(() => {
+  if (borough.value) {
+    return badges.filter((badge) => badge.borough === borough.value);
+  }
+  return badges;
+});
 </script>
 
 <style scoped>
-.new-badge {
+.header {
   position: absolute;
   top: var(--space-md);
   right: var(--space-md);
+  display: flex;
+  align-items: center;
+  gap: var(--space-md);
 }
 
 .badge-gallery {
