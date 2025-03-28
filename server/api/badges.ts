dffc09d71753d4f844074c74e89a54a43796858e
Nico Coloma-Cook <ncoloma-cook@justworks.com>
2025-03-28 14:04:54 -0400

    sort gallery by newest

--- server/api/badges.ts
+++ server/api/badges.ts
@@ -1,6 +1,7 @@
 import { db } from "~/db";
+import { desc } from "drizzle-orm";
 import { badgesTable } from "~/db/schema";
 
 export default defineEventHandler(async (event) => {
-  return await db.select().from(badgesTable);
+  return await db.select().from(badgesTable).orderBy(desc(badgesTable.id));
 });
